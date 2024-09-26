dca5916fe5e386863b4be5c7690a5dd5ad0d66b0
PickleJimy <dtcscience@gmail.com>
2024-09-26 16:45:28 -0500

    I LOVE MY RATS GAHOOEY

--- Gribbys Radical Robbery/Assets/SCRIPTS/World Scripts/Rat Stuff/RatPidDamage.cs
+++ Gribbys Radical Robbery/Assets/SCRIPTS/World Scripts/Rat Stuff/RatPidDamage.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class RatPidDamage : MonoBehaviour
 {
+    float elapsed = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        elapsed += Time.deltaTime;
+        if (elapsed >= 0.5f)
+        {
+            elapsed = elapsed % 0.5f;
+            gameObject.GetComponent<EnemyDealDamage>().dealDamage = !gameObject.GetComponent<EnemyDealDamage>().dealDamage;
+        }
     }
 }
