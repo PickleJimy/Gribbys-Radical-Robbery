ecbd143da9b135c5a5840ef5f8d90f069f603d0a
Phoblex <143538448+Phoblex@users.noreply.github.com>
2024-12-17 11:07:51 -0600

    Fixing Enemy Attack Movement.

--- Gribbys Radical Robbery/Assets/AttackMovement.cs
+++ Gribbys Radical Robbery/Assets/AttackMovement.cs
@@ -1,26 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AttackMovement : MonoBehaviour
 {
     // It will move during attack
     Rigidbody enemyRig;
-
+    public Transform player;
     public float magnitude;
     public void Awake()
     {
-
+        player = GameObject.Find("Player").transform;
     }
     void Start()
     {
-        enemyRig = GetComponent<Rigidbody>();
-        enemyRig.AddForce(Vector3.forward*magnitude,ForceMode.Impulse);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 a = transform.position;
+        Vector3 b = player.position;
+
+        enemyRig = GetComponent<Rigidbody>();
+        enemyRig.AddForce(Vector3.MoveTowards(a, b, magnitude));
+        transform.position = a;
     }
 }
