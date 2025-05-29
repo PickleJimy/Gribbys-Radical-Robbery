be6c46543e102160cb71b6319f7c9f0e46f4851e
PickleJimy <dtcscience@gmail.com>
2025-05-29 17:01:34 -0500

    DASH INDICATOR IS SO GOOD I LOVE IT

--- Gribbys Radical Robbery/Assets/SCRIPTS/UI Scipts/DashIndicator.cs
+++ Gribbys Radical Robbery/Assets/SCRIPTS/UI Scipts/DashIndicator.cs
@@ -1,30 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class DashIndicator : MonoBehaviour
 {
     public GameObject gribbyFace;
 
+    public Image gribbyImage;
+
     private bool canDash;
+
+    public float maximum;
+
+    private float current;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         canDash = PlayerMovement.readyToDash;
 
         if (!canDash)
+        {
             gribbyFace.SetActive(true);
 
+            current -= 0.1f;
+
+            SetFill();    
+        }
+
+
         if (canDash)
+        {
             gribbyFace.SetActive(false);
 
+            current = maximum;
+
+            SetFill();
+        }
+
+    }
+
+    void SetFill()
+    {
+        float fillAmount = (float)current / (float)maximum;
+        gribbyImage.fillAmount = fillAmount;
     }
 }
