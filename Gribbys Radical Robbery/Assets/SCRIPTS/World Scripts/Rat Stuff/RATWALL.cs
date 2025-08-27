99c93b1e59da889a32b98cf58ad80caf64aee7bc
PickleJimy <dtcscience@gmail.com>
2025-08-26 19:36:05 -0500

    cave pretty much done gameplay wise
    need me some outside assets and stuff and cave in stuff and other quality of life stuff

--- Gribbys Radical Robbery/Assets/SCRIPTS/World Scripts/Rat Stuff/RATWALL.cs
+++ Gribbys Radical Robbery/Assets/SCRIPTS/World Scripts/Rat Stuff/RATWALL.cs
@@ -4,43 +4,66 @@ using UnityEngine;
 
 public class RATWALL : MonoBehaviour
 {
+    [Header("Speed!!!")]
     public float speed;
-    public float rotationSpeed;
+    private float startSpeed;
+    //private float acceleration = 0.001f;
 
+    private float rotationTime;
+    private float dist;
+    
+    [Header("object references")]
     public GameObject rats;
+    public GameObject player;
     public GameObject[] paths;
 
     private int i = 0;
     private Vector3 startPos;
+    private Quaternion startRot;
 
     // Start is called before the first frame update
     void Start()
     {
         startPos = rats.transform.position;
+        startRot = rats.transform.rotation;
+        startSpeed = speed;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
        
-        Vector3 goal = paths[i].transform.position;
+        Transform goal = paths[i].transform;
 
-        Vector3 targetDirection = goal - rats.transform.position;
-        float step = rotationSpeed * Time.deltaTime;
-        Vector3 newDirection = Vector3.RotateTowards(rats.transform.forward, targetDirection, step, 0);
-        rats.transform.rotation = Quaternion.LookRotation(newDirection);
+        rats.transform.rotation = Quaternion.Slerp(startRot, goal.rotation, rotationTime);
 
-        if (rats.transform.position != goal)
+        //speed += acceleration;
+
+        if(Vector3.Distance(player.transform.position, rats.transform.position) < 3)
         {
-            float dist = Vector3.Distance(startPos, goal);
-
-            rats.transform.position = Vector3.MoveTowards(rats.transform.position, goal, dist / speed);
+            speed = 0;
         }
 
-        if (rats.transform.position == goal)
+        if(Vector3.Distance(player.transform.position, rats.transform.position) >= 3)
+        {
+            speed = startSpeed;
+        }
+
+        if (rats.transform.position != goal.position)
+        {
+            rats.transform.position = Vector3.MoveTowards(rats.transform.position, goal.position, 10 * (speed/100));
+
+            dist = Vector3.Distance(startPos, goal.position);
+
+            rotationTime = Vector3.Distance(startPos, rats.transform.position) / dist;
+        }
+
+        if (rats.transform.position == goal.position)
         {
             i++;
             startPos = rats.transform.position;
+            startRot = rats.transform.rotation;
+            
         }
     }
 }
